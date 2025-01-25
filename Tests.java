16111a527fc330211f76ca5dcb1b64569c99275a
DebbieMJ73 <tressore@att.net>
2025-01-24 23:59:33 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,19 @@
+//Debbie Johnson
+
+public class Tests {
+    public static void main(String[]arg){
+
+        double num1 = 76.5;
+        double num2 = 89.2;
+        double num3 = 92.8;
+
+        double avrgNum = (num1 + num2 + num3)/3;
+
+        System.out.println("Test score 1: " + num1);
+        System.out.println("Test score 2: " + num2);
+        System.out.println("Test score 3: " + num3);
+        System.out.println("The average of three test scores is: " + avrgNum);
+
+
+    }
+}
