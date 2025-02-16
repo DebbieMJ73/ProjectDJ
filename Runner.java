ed253a72292041e9ff3b31d5ebc6d62fe275c005
DebbieMJ73 <tressore@att.net>
2025-02-16 15:06:47 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,39 +1,15 @@
-import java.util.Scanner;
-
 public class Runner {
-    //main
-    public static void main(String[] args) {
-
-        //Instantiate Pet
-        Pet myPet1 = new Pet();
-        Pet myPet2 = new Pet();
-        myPet2.setName("Sandy");
-        myPet2.setAge(15);
-        myPet2.setType("Dog");
-
-        //Print pet information
-        System.out.println( myPet1);
-        System.out.println(myPet2 );
-
-        //Get user input for  third pet with scanner
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter pet's name:  ");
-        String name = scan.nextLine();
-
-        System.out.println("Enter pet's age from 1 to 100: ");
-        int age = scan.nextInt();
-
-        System.out.println("Enter pet's type (cat, dog, bird, etc.): ");
-        String type = scan.next();
+    public static void main(String[] arg) {
 
 
-        //Create third pet
-        Pet myPet3 = new Pet(name, age, type);
-        System.out.println(myPet3);
+        //Instantiate an object to our Tests class using constructor
+        Tests myScores = new Tests();
+        System.out.println(myScores.toString());
 
-        //Close the scanner
-        scan.close();
+        //Call getAverage method
+        myScores.getAverage();
+
+
 
     }
 }
--- Tests.java
+++ Tests.java
@@ -1,19 +1,82 @@
 //Debbie Johnson
+import java.util.Scanner;
+
+
 
 public class Tests {
-    public static void main(String[]arg){
+    private double ave;
+    private int count;
+    private int score;
 
-        double num1 = 76.5;
-        double num2 = 89.2;
-        double num3 = 92.8;
 
-        double avrgNum = (num1 + num2 + num3)/3;
-
-        System.out.println("Test score 1: " + num1);
-        System.out.println("Test score 2: " + num2);
-        System.out.println("Test score 3: " + num3);
-        System.out.println("The average of three test scores is: " + avrgNum);
+    //public Constructors
+    public Tests() {
+        this.ave = 0.0;
+        this.count = 0;
+        this.score = 0;
 
 
     }
+
+    // public set and get methods for every private data field
+    public double getAve() {
+        return this.ave;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    //public toString method that prints out all the objects state
+    public String toString() {
+
+        String output = "Result: \n";
+        output += ("The average of the " + this.getCount() + " scores " + " entered is " + this.getAve());
+
+        return output;
+
+
+    }
+
+    public void getAverage() {
+        //Get user input for test scores
+        Scanner scan = new Scanner(System.in);
+
+        //Prompt user to enter test scores
+        System.out.println("Please enter test scores (type -1 to quit): ");
+
+        //Read test scores
+        this.score = scan.nextInt();
+
+        int  sum = 0;
+
+        //while loop to accept score until -1 is entered
+        while (this.score >= 1) {
+
+            int sum1 = sum;
+            sum1 += this.score;
+            int i = this.count;
+            i++;
+
+
+            System.out.println("Please enter test scores (type -1 to quit): ");
+            this.score = scan.nextInt();
+
+
+            //Calculate average
+            this.ave = (double) sum1 / i;
+
+        }
+
+
+    }
+
 }
